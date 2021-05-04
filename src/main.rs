f32b5be33387aaf78f0390388f0d800160a3d789
Ivan Towlson <github@hestia.cc>
2021-05-04 12:19:46 +1200

    Refactor arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,22 @@ mod expander;
 mod hippofacts;
 mod invoice;
 
+const ARG_HIPPOFACTS: &str = "hippofacts_path";
+const ARG_INVOICE: &str = "invoice_path";
+
 fn main() -> anyhow::Result<()> {
-    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    let args = clap::App::new("hippofactory")
+        .version("0.0.1")
+        .author("Deis Labs")
+        .arg(clap::Arg::new(ARG_HIPPOFACTS).required(true).index(1).about("The artifacts spec"))
+        .arg(clap::Arg::new(ARG_INVOICE).required(true).index(2).about("The invoice file to generate"))
+        .get_matches();
 
-    // For simplicity we start out requiring both input and output files -
-    // *we will relax this restriction*!
-    if args.len() != 2 {
-        println!("Usage: hippofactory source-file destination-file");
-        return Ok(());
-    }
+    let hippofacts_arg = args.value_of(ARG_HIPPOFACTS).ok_or(anyhow::Error::msg("HIPPOFACTS file is required"))?;
+    let invoice_arg = args.value_of(ARG_INVOICE).ok_or(anyhow::Error::msg("Invoice path is required"))?;
 
-    let source = std::env::current_dir()?.join(&args[0]);
-    let destination = std::env::current_dir()?.join(&args[1]);
+    let source = std::env::current_dir()?.join(hippofacts_arg);
+    let destination = std::env::current_dir()?.join(invoice_arg);
 
     run(&source, &destination)
 }

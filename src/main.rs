e7e6b7fb405d1190ac3cd79e82252cc4a269f2f8
Ivan Towlson <github@hestia.cc>
2021-05-07 12:18:02 +1200

    Command line options to control push

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ mod invoice;
 const ARG_HIPPOFACTS: &str = "hippofacts_path";
 const ARG_STAGING_DIR: &str = "staging_dir";
 const ARG_VERSIONING: &str = "versioning";
+const ARG_SERVER_URL: &str = "bindle_server";
+const ARG_PREPARE_ONLY: &str = "prepare";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -39,6 +41,21 @@ async fn main() -> anyhow::Result<()> {
                 .long("invoice-version")
                 .about("How to version the generated invoice"),
         )
+        .arg(
+            clap::Arg::new(ARG_SERVER_URL)
+                .required_unless_present(ARG_PREPARE_ONLY)
+                .short('s')
+                .long("server")
+                .env("BINDLE_SERVER_URL")
+                .about("The Bindle server to push the artifacts to")
+        )
+        .arg(
+            clap::Arg::new(ARG_PREPARE_ONLY)
+                .required(false)
+                .takes_value(false)
+                .long("prepare")
+                .about("Prepares an artifact layout but does not push"),
+        )
         .get_matches();
 
     let hippofacts_arg = args
@@ -48,20 +65,26 @@ async fn main() -> anyhow::Result<()> {
         .value_of(ARG_STAGING_DIR)
         .ok_or_else(|| anyhow::Error::msg("Staging directory is required"))?;
     let versioning_arg = args.value_of(ARG_VERSIONING).unwrap();
+    let push_to =
+        if args.is_present(ARG_PREPARE_ONLY) {
+            None
+        } else {
+            Some(args.value_of(ARG_SERVER_URL).ok_or_else(|| anyhow::Error::msg("Server URL is required"))?.to_owned())
+        };
 
     let source = std::env::current_dir()?.join(hippofacts_arg);
     let destination = std::env::current_dir()?.join(invoice_arg);
     let invoice_versioning = InvoiceVersioning::parse(versioning_arg);
 
-    run(&source, &destination, invoice_versioning).await
+    run(&source, &destination, invoice_versioning, push_to).await
 }
 
 async fn run(
     source: impl AsRef<std::path::Path>,
     destination: impl AsRef<std::path::Path>,
     invoice_versioning: InvoiceVersioning,
+    push_to: Option<String>,
 ) -> anyhow::Result<()> {
-    let bindle_server_url = "http://localhost:8080/v1";
     let source_dir = source
         .as_ref()
         .parent()
@@ -77,9 +100,14 @@ async fn run(
     let spec = toml::from_str::<HippoFacts>(&content)?;
     let invoice = expander::expand(&spec, &expansion_context)?;
     writer.write(&invoice).await?;
-    bindle_pusher::push_all(&destination, invoice.id()?, bindle_server_url).await?;
 
-    println!("pushed: {}/{}", &invoice.bindle.name, &invoice.bindle.version);
+    if let Some(url) = push_to {
+        bindle_pusher::push_all(&destination, invoice.id()?, &url).await?;
+        println!("pushed: {}/{}", &invoice.bindle.name, &invoice.bindle.version);
+    } else {
+        println!("id:      {}/{}", &invoice.bindle.name, &invoice.bindle.version);
+        println!("command: bindle push -p {} {}/{}", &destination.as_ref().canonicalize()?.to_string_lossy(), &invoice.bindle.name, &invoice.bindle.version);
+    }
 
     Ok(())
 }

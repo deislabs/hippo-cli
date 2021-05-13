5655c0686590de8d2953dc94d9ba517843a81eed
itowlson <ivan.towlson@microsoft.com>
2021-05-13 13:04:36 +1200

    Fix issue where two files with same content at different paths would have been treated as the same parcel when deduplication

--- src/expander.rs
+++ src/expander.rs
@@ -205,7 +205,7 @@ fn convert_one_match_to_parcel(
 fn merge_memberships(parcels: Vec<Parcel>) -> Vec<Parcel> {
     parcels
         .into_iter()
-        .into_grouping_map_by(|p| p.label.sha256.clone())
+        .into_grouping_map_by(file_id)
         .fold_first(|acc, _key, val| merge_parcel_into(acc, val))
         .values()
         .cloned() // into_values is not yet stable
@@ -275,6 +275,12 @@ fn current_user() -> Option<String> {
 
 fn no_handlers() -> anyhow::Error {
     anyhow::anyhow!("No handlers defined in artifact spec")
+}
+
+fn file_id(parcel: &Parcel) -> String {
+    // Two parcels with different names could refer to the same content.  We
+    // don't want to treat them as the same parcel when deduplicating.
+    format!("{}@{}", parcel.label.sha256, parcel.label.name)
 }
 
 #[cfg(test)]

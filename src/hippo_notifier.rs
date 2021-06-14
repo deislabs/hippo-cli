9da892835d72aa5c10e940a47a61e3bbae397bc8
Ivan Towlson <github@hestia.cc>
2021-06-15 11:46:21 +1200

    Fixed TODO comment

--- src/hippo_notifier.rs
+++ src/hippo_notifier.rs
@@ -5,7 +5,6 @@ pub struct ConnectionInfo {
 }
 
 pub async fn register(bindle_id: &bindle::Id, conn_info: &ConnectionInfo) -> anyhow::Result<()> {
-    // TODO: username and password
     let hippo_client = hippo::Client::new_from_login(&conn_info.url, &conn_info.username, &conn_info.password).await?;
     hippo_client
         .register_revision_by_storage_id(bindle_id.name(), &bindle_id.version_string())

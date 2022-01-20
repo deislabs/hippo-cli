973787c86132eacf1fd18af94882e8ec318e752e
Matthew Fisher <matt.fisher@fermyon.com>
2022-01-20 13:24:46 -0800

    switch to hippo-openapi
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- src/hippo_notifier.rs
+++ src/hippo_notifier.rs
@@ -1,3 +1,15 @@
+use hippo_openapi::apis::configuration::ApiKey;
+use hippo_openapi::apis::{
+    account_api::api_account_createtoken_post,
+    configuration::Configuration,
+    revision_api::api_revision_post
+};
+use hippo_openapi::models::{ CreateTokenCommand, RegisterRevisionCommand };
+
+use reqwest::header;
+
+const JSON_MIME_TYPE: &str = "application/json";
+
 pub struct ConnectionInfo {
     pub url: String,
     pub danger_accept_invalid_certs: bool,
@@ -6,49 +18,41 @@ pub struct ConnectionInfo {
 }
 
 pub async fn register(bindle_id: &bindle::Id, conn_info: &ConnectionInfo) -> anyhow::Result<()> {
-    let options = hippo::ClientOptions {
-        danger_accept_invalid_certs: conn_info.danger_accept_invalid_certs,
-    };
-    let hippo_client = hippo::Client::new_with_options(
-        &conn_info.url,
-        &conn_info.username,
-        &conn_info.password,
-        options,
-    )
-    .await?;
-    hippo_client
-        .register_revision_by_storage_id(bindle_id.name(), &bindle_id.version_string())
-        .await
-        .map_err(format_register_revision_error)
-}
+    
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::ACCEPT, JSON_MIME_TYPE.parse().unwrap());
+    headers.insert(header::CONTENT_TYPE, JSON_MIME_TYPE.parse().unwrap());
 
-fn format_register_revision_error(e: hippo::ClientError) -> anyhow::Error {
-    let message = match &e {
-        hippo::ClientError::InvalidRequest { status_code, message } => {
-            let detail_clause = match message {
-                Some(m) => format!(": error was {}", m),
-                None => "".to_owned(),
-            };
-            match status_code {
-                &reqwest::StatusCode::BAD_REQUEST =>
-                    format!("Hippo couldn't understand the request{} (400 Bad Request)", detail_clause),
-                &reqwest::StatusCode::UNAUTHORIZED =>
-                    format!("Login failed: please check your credentials{} (401 Unauthorized)", detail_clause),
-                &reqwest::StatusCode::FORBIDDEN =>
-                    format!("Hippo can't register this revision{} (403 Forbidden)", detail_clause),
-                &reqwest::StatusCode::NOT_FOUND =>
-                    "You don't have access to any applications that use this bindle ID (404 Not Found)".to_owned(),
-                &reqwest::StatusCode::METHOD_NOT_ALLOWED =>
-                    // At one point this could be returned for TLS mismatch; not sure if we fixed that
-                    format!("Wrong HTTP method; may also indicate you need to use/turn off HTTPS{} (405 Method Not Allowed)", detail_clause),
-                &reqwest::StatusCode::CONFLICT =>
-                    format!("Revision conflicts with existing Hippo state; it might already exist{} (409 Conflict)", detail_clause),
-                &reqwest::StatusCode::IM_A_TEAPOT =>
-                    "Specified URL is for a teapot not a Hippo (418 I'm a Teapot)".to_owned(),
-                _ => e.to_string(),
-            }
-        },
-        _ => e.to_string(),
+    let mut configuration = Configuration {
+        base_path: conn_info.url.clone(),
+        user_agent: Some(format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))),
+        client: reqwest::Client::builder().danger_accept_invalid_certs(conn_info.danger_accept_invalid_certs).default_headers(headers).build()?,
+        basic_auth: None,
+        oauth_access_token: None,
+        bearer_access_token: None,
+        api_key: None,
     };
-    anyhow::anyhow!("Error registering revision: {}", message)
+    
+    let token_info = api_account_createtoken_post(
+        &configuration,
+        Some(CreateTokenCommand {
+            user_name: Some(conn_info.username.clone()),
+            password: Some(conn_info.password.clone())
+        })
+    ).await?;
+
+    configuration.api_key = Some(ApiKey {
+        prefix: Some("Bearer".to_owned()),
+        key: token_info.token.unwrap(),
+    });
+
+    api_revision_post(
+        &configuration,
+        Some(RegisterRevisionCommand {
+            app_storage_id: Some(bindle_id.name().to_owned()),
+            revision_number: Some(bindle_id.version_string())
+        })
+    ).await?;
+
+    Ok(())
 }
